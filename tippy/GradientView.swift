d751ad42758c0131f2b4137a879ca7540d7c16d5
johnyu763 <johny11@uci.edu>
2019-08-21 20:53:54 -0700

    default view

---
+++ tippy/GradientView.swift
@@ -0,0 +1,45 @@
+//
+//  GradientView.swift
+//  tippy
+//
+//  Created by John Yu on 8/21/19.
+//  Copyright © 2019 John Yu. All rights reserved.
+//
+import UIKit
+
+@IBDesignable class GradientView: UIView
+{
+    @IBInspectable var gradientColor1: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientColor2: UIColor = UIColor.white {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientStartPoint: CGPoint = .zero {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    @IBInspectable var gradientEndPoint: CGPoint = CGPoint(x: 0, y: 1) {
+        didSet{
+            self.setGradient()
+        }
+    }
+    
+    private func setGradient()
+    {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [self.gradientColor1.cgColor, self.gradientColor2.cgColor]
+        gradientLayer.startPoint = self.gradientStartPoint
+        gradientLayer.endPoint = self.gradientEndPoint
+        gradientLayer.frame = self.bounds
+        self.layer.addSublayer(gradientLayer)
+    }
+}
