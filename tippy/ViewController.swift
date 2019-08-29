b35e29fae19cab98b2b01bd2ce572587d5b0ffac
johnyu763 <johny11@uci.edu>
2019-08-28 22:54:35 -0700

    pull before merge

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var tipPercentages: [UIButton]!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet var gradView: GradientView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,6 +28,37 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func handleSelection(_ sender: UIButton) {
+        tipPercentages.forEach{button in
+            button.isHidden = !button.isHidden
+        }
+    }
+    
+    enum Percents: String {
+        case fifteen = "15%"
+        case eighteen = "18%"
+        case twenty = "20%"
+    }
+    
+    @IBAction func percentTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let percent = Percents(rawValue: title) else
+        {
+            return
+        }
+        let defaults = UserDefaults.standard
+        switch percent{
+        case .fifteen:
+            defaults.set(0, forKey: "myIndex")
+        case .eighteen:
+            defaults.set(1, forKey: "myIndex")
+        case .twenty:
+            defaults.set(2, forKey: "myIndex")
+        }
+    }
+    
+    @IBAction func handleMain1ColorChange(_ sender: UISlider) {
+        
+    }
     
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
